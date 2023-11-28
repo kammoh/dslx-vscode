ac9b8749bfeef3fd45fecd860564bb64525c47ca
Kamyar Mohajerani <kammoh@gmail.com>
2023-11-27 20:03:03 -0500

    add server options, cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,42 @@
 /* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * Copyright 2023 (c) Kamyar Mohajerani. All rights reserved.
+ * Licensed under the Apache 2.0 License.
+ * See the included LICENSE file in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from 'path';
+import * as vscode from 'vscode';
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
     LanguageClient,
     LanguageClientOptions,
+    Executable,
     ServerOptions,
     TransportKind
 } from 'vscode-languageclient/node';
 
-let client: LanguageClient;
+let client: undefined | LanguageClient = undefined;
 
 export function activate(context: ExtensionContext) {
-    // The server is implemented in node
-    // const serverModule = context.asAbsolutePath(
-    //     path.join('server', 'out', 'server.js')
-    // );
-    const serverExecutable = 'dslx_ls';
+    const output = vscode.window.createOutputChannel('DSLX Language Server');
+    const config = vscode.workspace.getConfiguration('dslx');
+    const binary_path: string = config.get('languageServerPath') as string;
+    const dslx_path: string = config.get('dslxPath') as string;
+    const stdlib_path: string = config.get('stdlibPath') as string;
 
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        command: serverExecutable,
+    const serverOptions: Executable = {
+        command: binary_path,
         transport: TransportKind.stdio,
-        args: ["--"],
+        args: ["--dslx_path", dslx_path, "--stdlib_path", stdlib_path, "--"],
     };
 
     // Options to control the language client
     const clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'dslx'}],
-        synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-        }
+        documentSelector: [{ scheme: 'file', language: 'dslx' }],
+        outputChannel: output,
     };
 
     // Create the language client and start the client.
